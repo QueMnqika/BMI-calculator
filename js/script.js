ddf081638916af46781f394a8b4b701495c81122
Que Mnqika <qaqambilemnqika23@gmail.com>
2023-11-13 17:11:12 +0200

    push bmi

---
+++ js/script.js
@@ -0,0 +1,18 @@
+let height = document.querySelector('#userInput')
+let weight = document.querySelector('#userInput2')
+let convert = document.querySelector('#buttons')
+let output = document.querySelector('#output')
+let reset = document.querySelector('#reset')
+
+convert.addEventListener('click', () => {
+    let results = height.value + weight.value
+    output.textContent = `The results are ${results}`
+})
+
+reset.addEventListener('click', ()=>{
+    if (height.value != '') {
+        height.value = ''
+    } else if (weight.value != '') {
+        weight.value = ''
+    }
+})
